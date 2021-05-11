2cd87fd10d9a929034c6c73851b367b8f2b5b969
Santiago Manez <santiago@devdot.co>
2021-05-11 20:20:10 -0300

    add .env.example

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -10,7 +10,7 @@
         <div class="container-fluid">
         	<h1 class="text-center mt-4 mb-4">Ejercicio Técnico Bymovi</h1>
         	<div class="row">
-        	    <div class="col-12">
+        	    <div class="col-12 text-center">
         	        <a href="{{ URL::route('users.create') }}" class="btn btn-success mb-2">Crear Usuario</a>
         	        <a href="{{ URL::route('roles.create') }}" class="btn btn-success mb-2">Crear Rol</a>
         	        <a href="{{ URL::route('index') }}" class="btn btn-success mb-2">Listado de Usuarios</a>
